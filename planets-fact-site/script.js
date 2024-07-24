561bfd739db004c91e6cc1c1732c75de338b2ca8
Tom Roche <121121256+TomrocheDev@users.noreply.github.com>
2024-07-24 10:00:42 +0200

    fix mobile header

--- planets-fact-site/script.js
+++ planets-fact-site/script.js
@@ -56,7 +56,6 @@ async function createPage(planetName) {
 
 $(document).ready(function () {
     const currentPlanet = $("body").data("planet");
-    $("header").load("includes/header.html");
     createPage(currentPlanet);
 
     $(".button-group button").on("click", function () {
